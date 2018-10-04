ebdc57e102181fcaa0788bea0ae56f0a91d21c11
Ajawu David <ajawudavid@gmail.com>
2018-10-04 16:21:29 +0100

    finished SumOfMultiples function

--- src/main/scala/SumOfMultiples.scala
+++ src/main/scala/SumOfMultiples.scala
@@ -1,6 +1,18 @@
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int = {
-
-    1
+    var total = 0
+    def divisible(number: Int, facs: Set[Int]): Boolean={
+      for(arg <- facs){
+        if (number % arg == 0){
+          return true
+        }
+      }
+      return false
+    }
+    
+    for (count <- 1 to limit - 1){
+      if (divisible(count, factors)) total = total + count
+    }
+    total
   }
 }
