b1867481620d7c92cd0a7fafec93ad89813b5de2
Ajawu David <ajawudavid@gmail.com>
2018-10-05 15:51:11 +0100

    DifferenceOfSquares function completed

--- src/main/scala/DifferenceOfSquares.scala
+++ src/main/scala/DifferenceOfSquares.scala
@@ -1,8 +1,14 @@
 object DifferenceOfSquares{
 
-  def sumOfSquares(n: Int): Int = ???
+  def sumOfSquares(n: Int): Int = {
+    (1 to n).map(num => num * num).sum
+  }
 
-  def squareOfSum(n: Int): Int = ???
+  def squareOfSum(n: Int): Int = {
+    scala.math.pow((1 to n).sum, 2).toInt
+  }
 
-  def differenceOfSquares(n: Int):Int = ???
+  def differenceOfSquares(n: Int):Int = {
+    squareOfSum(n) - sumOfSquares(n)
+  }
 }
