7b30f744c4988f041f92686e7424ba06057fa36b
Ajawu David <ajawudavid@gmail.com>
2018-11-09 09:55:15 +0100

    gigasecond classwork done

--- src/main/scala/Gigasecond.scala
+++ src/main/scala/Gigasecond.scala
@@ -1,6 +1,14 @@
+import java.time.LocalDate
+import java.time.LocalDateTime
 object Gigasecond {
-  def add(startDate: LocalDate): LocalDateTime = ???
+  def add(startDate: LocalDate): LocalDateTime = {
+    val gigasecondMoment = startDate.atStartOfDay().plusSeconds(1000000000)
+    gigasecondMoment
+  }
 
-  def add(startDateTime: LocalDateTime): LocalDateTime = ???
+  def add(startDateTime: LocalDateTime): LocalDateTime = {
+    val gigasecondMoment = startDateTime.plusSeconds(1000000000)
+    gigasecondMoment
+  }
 }
 
